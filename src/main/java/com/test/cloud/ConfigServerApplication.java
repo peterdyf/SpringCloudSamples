333fa5afb13d296683b2d1203d6dd419f853e15f
DiYifei <diyifei@gmail.com>
2016-11-05 12:06:21 +0800

    add url comment

--- src/main/java/com/test/cloud/ConfigServerApplication.java
+++ src/main/java/com/test/cloud/ConfigServerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-
+//http://localhost:8001/config-client/default/
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
