e91c8419b7147252e6f9227b757b2dc1db81f64d
DiYifei <diyifei@gmail.com>
2016-11-12 12:47:20 +0800

    ribbon config

--- EurekaConsumer/src/main/java/com/test/eureka/EurekaConsumerController.java
+++ EurekaConsumer/src/main/java/com/test/eureka/EurekaConsumerController.java
@@ -2,7 +2,7 @@ package com.test.eureka;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -14,17 +14,11 @@ import java.util.List;
 public class EurekaConsumerController {
 
     @Autowired
-    DiscoveryClient client;
+    LoadBalancerClient client;
 
     @RequestMapping("/test")
     public  String test() {
-        List<ServiceInstance> list = client.getInstances("eureka-client-subject");
-        if (list != null && list.size() > 0 ) {
-            URI uri = list.get(0).getUri();
-            if (uri !=null ) {
-                return (new RestTemplate()).getForObject(uri,String.class);
-            }
-        }
-        return null;
+        ServiceInstance instance = client.choose("eureka-client-subject");
+        return (new RestTemplate()).getForObject(instance.getUri(),String.class);
     }
 }
